d4a143090090064178c43650e9806acc7dfd154a
Gerard Mortel <gmortel@us.ibm.com>
2023-11-28 10:22:59 -0600

    Added proof of good install

--- 05_install_helm.sh
+++ 05_install_helm.sh
@@ -4,3 +4,4 @@ echo "#### Download and install helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+helm version
