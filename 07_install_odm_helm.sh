b9c633f0183a8ec2dfc7b524eaa02dec78ab7900
Gerard Mortel <gmortel@us.ibm.com>
2023-12-01 17:03:15 -0600

    Paremeterizing release name during helm install

--- 07_install_odm_helm.sh
+++ 07_install_odm_helm.sh
@@ -15,22 +15,25 @@ if [ $INSTALLTYPE == "prod" ]; then
   echo "#### Start Prod install"
   echo "#### Create the Decision Center Persistent Volume Claim"
   cat <<EOF | kubectl apply -f -
-  apiVersion: v1
-  kind: PersistentVolumeClaim
-  metadata:
-    name: my-custom-dc-libs-pvc
-  spec:
-    accessModes:
-    - ReadWriteOnce
-    resources:
-      requests:
-        storage: 1Gi
-    storageClassName: ${STORAGECLASSNAME}
-    volumeMode: Filesystem
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: my-custom-dc-libs-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+  storageClassName: ${STORAGECLASSNAME}
+  volumeMode: Filesystem
 EOF
 
   echo "#### Install ibm-helm/ibm-odm-${INSTALLTYPE}"
-  # helm install ${RELEASENAME} \
+  helm install ${RELEASENAME} \
+      ibm-helm/ibm-odm-prod \
+      --values values.yaml
+
       # --set license=true \
       # --set image.repository=cp.icr.io/cp/cp4a/odm \
       # --set usersPassword=${USERSPASSWORD} \
@@ -40,16 +43,14 @@ EOF
       # --set internalDatabase.persistence.storageClassName=${STORAGECLASSNAME} \
       # --set decisionCenter.customlibPvc=my-custom-dc-libs-pvc \
       # --set customization.runAsUser=1000690000 \
-      # ibm-helm/ibm-odm-prod \
-      # --values values.yaml
 
-helm install odm1 ibm-helm/ibm-odm-prod --values values.yaml
+  # helm install odm1 ibm-helm/ibm-odm-prod --values values.yaml
 
-  echo "#### Copy custom jar to Decision Center"
-  pod=`(oc get pods | grep ${RELEASENAME} | awk '{print $1}')`
-  oc cp Gerard.jar $pod:/config/customlib
-  oc exec $pod -- ls -l /config/customlib
-  oc delete pod $pod
+  # echo "#### Copy custom jar to Decision Center"
+  # pod=`(oc get pods | grep decisioncenter | awk '{print $1}')`
+  # oc cp Gerard.jar $pod:/config/customlib
+  # oc exec $pod -- ls -l /config/customlib
+  # oc delete pod $pod
 
   echo "#### End Prod Install"
 
@@ -60,7 +61,7 @@ else
     helm install ${RELEASENAME} \
         --set license=accept \
         --set usersPassword=${USERSPASSWORD} \
-        ibm-charts/ibm-odm-${INSTALLTYPE}
+        ibm-charts/ibm-odm-dev
     
     echo "#### End Dev Install"
 fi
